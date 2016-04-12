4ba324d7684ae88bab9e77c691f0968ec7345ac8
tigerbombz <madd1778@yahoo.com>
2016-04-12 12:57:12 -0700

    added more notes to content page

--- src/components/content_index.js
+++ src/components/content_index.js
@@ -14,6 +14,8 @@ class ContentIndex extends Component {
     return (
       <div>
         <img src={'http://media.animevice.com/uploads/2/21764/364936-yoshi_1.jpg'} alt='yoshi' />
+        //add main pic here
+        //make new div for other pictures after
       </div>
     );
   }
